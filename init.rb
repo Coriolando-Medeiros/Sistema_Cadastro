64df3550fc33839932fc2bc774a71ec0bdf1a577
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-09-28 07:39:26 -0300

    feat: adicionar produtos no hash produtos

--- init.rb
+++ init.rb
@@ -1,6 +1,34 @@
 require_relative 'display/menu'
 require_relative 'display/operacoes_de_tela'
 require_relative 'core/cadastro_produto'
+require_relative 'core/listar_produtos'
 
-produtos = []
+produtos = [
+    {
+        id: Time.now.to_i,
+        nome: "Maça",
+        descricao: "Maça da turma da Mônica",
+        preco: 2.75,
+        quantidade: 50
+    },
+
+    {
+        id: Time.now.to_i,
+        nome: "Manga",
+        descricao: "Manga Rosa",
+        preco: 3.80,
+        quantidade: 60
+    },
+
+    {
+        id: Time.now.to_i,
+        nome: "Uva",
+        descricao: "Uva Niagara",
+        preco: 8.95,
+        quantidade: 80
+    }
+]
+
 iniciar_menu(produtos)
+
+#parei no minuto 50:00
