ccb04e9951dd336b46de5d64b5d01b3f4f1a2b3b
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-10-04 10:14:07 -0300

    feat: adequação do código para rodar sem erros

--- init.rb
+++ init.rb
@@ -1,34 +1,14 @@
 require_relative 'display/menu'
 require_relative 'display/operacoes_de_tela'
-require_relative 'core/cadastro_produto'
-require_relative 'core/listar_produtos'
-require_relative 'core/retirar_estoque'
+require_relative 'display/servicos/cadastro_produto'
+require_relative 'display/servicos/listar_produtos'
+require_relative 'display/servicos/retirar_estoque'
+require_relative 'dominio/entidades/produto'
+require_relative 'dominio/servicos/produto_servico'
+require_relative 'infraestrutura/json_repositorio'
 
-produtos = [
-    {
-        id: 1,#Time.now.to_i,
-        nome: "Maça",
-        descricao: "Maça da turma da Mônica",
-        preco: 2.75,
-        quantidade: 50
-    },
 
-    {
-        id: 2, #Time.now.to_i,
-        nome: "Manga",
-        descricao: "Manga Rosa",
-        preco: 3.80,
-        quantidade: 60
-    },
 
-    {
-        id: 3, #Time.now.to_i,
-        nome: "Uva",
-        descricao: "Uva Niagara",
-        preco: 8.95,
-        quantidade: 80
-    }
-]
+iniciar_menu
 
-iniciar_menu(produtos)
 
