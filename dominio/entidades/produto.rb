3020350b925e5f19ea4df28ede84aca7a63c7ed2
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-10-04 10:12:00 -0300

    feat: altera a classe Produto para aceitar hash

--- dominio/entidades/produto.rb
+++ dominio/entidades/produto.rb
@@ -1,10 +1,10 @@
 class Produto #Classe anêmica
     def initialize(produto_hash)
-        self.id = produto_hash[:id]
-        self.nome = produto_hash[:nome]
-        self.descricao = produto_hash[:descricao]
-        self.preco = produto_hash[:preco]
-        self.quantidade = produto_hash[:quantidade]
+        self.id = produto_hash["id"]
+        self.nome = produto_hash["nome"]
+        self.descricao = produto_hash["descricao"]
+        self.preco = produto_hash["preco"]
+        self.quantidade = produto_hash["quantidade"]
     end
 
     attr_accessor :id, :nome, :descricao, :preco, :quantidade
